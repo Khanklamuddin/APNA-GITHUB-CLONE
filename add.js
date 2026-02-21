3de5095319db42615fdb7ee74c0f59be40da8a52
Klamuddin Khan <kklamuddin90@gmail.com>
2026-02-21 16:28:49 +0530

    Add files via upload

--- add.js
+++ add.js
@@ -8,7 +8,7 @@ async function addRepo(filePath){
     const stagingPath = path.join(repoPath, "staging");
 
     try{
-        await fs.mkdir(stagingPath, { recursive:true});
+        await fs.mkdir(stagingPath, { recursive:true });
         const fileName = path.basename(filePath);
         await fs.copyFile(filePath, path.join(stagingPath, fileName));
         console.log(`file ${fileName} added to the staging area`);
